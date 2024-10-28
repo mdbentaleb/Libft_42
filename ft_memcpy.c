1fb1a3d641655c1543d8cfd435e15f17b643d282
Mohamed Ben taleb <moben-ta@c2r5p3.1337.ma>
2024-10-28 15:30:16 +0100

    ***

---
+++ ft_memcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moben-ta <moben-ta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 14:30:50 by moben-ta          #+#    #+#             */
+/*   Updated: 2024/10/28 15:29:43 by moben-ta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	unsigned char *tmp_dst;
+	unsigned char *tmp_src;
+	size_t	i;
+
+	if (dst == NULL || src == NULL)
+		return (dst);
+	tmp_dst = (unsigned char *)dst;
+	tmp_src = (unsigned char *)src;
+	i = 0;
+	while (i < n)
+	{
+		tmp_dst[i] = tmp_src[i];
+		i++;
+	}
+	return (dst);
+}
