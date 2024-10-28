7b2b0941377794fbfe5f768540bf7ada96adc4d1
Mohamed Ben taleb <moben-ta@c2r5p3.1337.ma>
2024-10-28 14:34:47 +0100

    -

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: moben-ta <moben-ta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 10:38:23 by moben-ta          #+#    #+#             */
-/*   Updated: 2024/10/28 12:52:58 by moben-ta         ###   ########.fr       */
+/*   Updated: 2024/10/28 14:33:44 by moben-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,13 @@ int		ft_atoi(const char *str);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
-//size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 void 	*ft_memchr(const void *s, int c, size_t n);
 void 	*ft_calloc(size_t count, size_t size);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+void	*memcpy(void *restrict dst, const void *restrict src, size_t n);
 
 #endif
