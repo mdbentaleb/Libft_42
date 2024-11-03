94b1f8682a74dc8cf7c3d2e9709bc817b9e2193d
Mohamed Ben taleb <moben-ta@c2r5p3.1337.ma>
2024-11-03 17:17:10 +0100

    -

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: moben-ta <moben-ta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 10:38:23 by moben-ta          #+#    #+#             */
-/*   Updated: 2024/11/03 14:13:45 by moben-ta         ###   ########.fr       */
+/*   Updated: 2024/11/03 16:44:23 by moben-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,5 +62,6 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 
 #endif
