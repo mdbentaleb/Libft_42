8fea1bf225d8cfc87e9f3a461b99303fee84a3dc
Mohamed Ben taleb <moben-ta@c2r5p3.1337.ma>
2024-11-03 16:35:00 +0100

    -

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -1,18 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_back_b                                   :+:      :+:    :+:   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: moben-ta <moben-ta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 14:06:24 by moben-ta          #+#    #+#             */
-/*   Updated: 2024/11/03 15:36:41 by moben-ta         ###   ########.fr       */
+/*   Updated: 2024/11/03 16:14:10 by moben-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "./libft.h"
+
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	if (!list || !new)
+	t_list	*tmp;
+
+	if (!lst || !new)
 		return ;
-	
+	if (*lst == NULL)
+		*lst = new;
+	else
+	{
+		tmp = *lst;
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+		tmp->next = new;
+	}
 }
