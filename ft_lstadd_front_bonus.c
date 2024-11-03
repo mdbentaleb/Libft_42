f968fb0a2992593c9131167ed0fba8f61a604f96
Mohamed Ben taleb <moben-ta@c2r5p3.1337.ma>
2024-11-03 09:12:51 +0100

    -

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -6,7 +6,7 @@
 /*   By: moben-ta <moben-ta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 12:27:56 by moben-ta          #+#    #+#             */
-/*   Updated: 2024/11/03 07:55:22 by moben-ta         ###   ########.fr       */
+/*   Updated: 2024/11/03 09:11:25 by moben-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,8 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	
+	if (!lst || !new)
+		return ;
+	new->next = *lst;
+	*lst = new;
 }
