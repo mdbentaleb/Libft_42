211f4eb0c29127f327aeeedb4cb4cf43e1420c2f
Mohamed Ben taleb <moben-ta@c2r5p3.1337.ma>
2024-11-04 09:20:50 +0100

    -

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,9 +6,11 @@
 /*   By: moben-ta <moben-ta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 16:38:15 by moben-ta          #+#    #+#             */
-/*   Updated: 2024/11/03 16:55:50 by moben-ta         ###   ########.fr       */
+/*   Updated: 2024/11/04 08:25:36 by moben-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "./libft.h"
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
