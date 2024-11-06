311b6cfe0c940952d743491d46d93c519e4be4f8
Mohamed Ben taleb <moben-ta@c2r5p3.1337.ma>
2024-11-06 09:47:59 +0100

    -

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: moben-ta <moben-ta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 16:03:18 by moben-ta          #+#    #+#             */
-/*   Updated: 2024/11/01 11:43:31 by moben-ta         ###   ########.fr       */
+/*   Updated: 2024/11/06 09:43:08 by moben-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	tmp = (char *)malloc((s1_len + s2_len) * sizeof(char) + 1);
 	if (!tmp)
 		return (NULL);
-	ft_strlcat(tmp, s1, s1_len + 1);
+	ft_strlcpy(tmp, s1, s1_len + 1);
 	ft_strlcat(tmp + s1_len, s2, s2_len + 1);
 	return (tmp);
 }
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: moben-ta <moben-ta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 09:41:48 by moben-ta          #+#    #+#             */
-/*   Updated: 2024/11/05 16:20:15 by moben-ta         ###   ########.fr       */
+/*   Updated: 2024/11/06 09:14:49 by moben-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	return (0);
