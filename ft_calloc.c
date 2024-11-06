fcd5cb7ccf4187330c2f259a55dd595675c0a09e
Mohamed Ben taleb <moben-ta@c2r5p3.1337.ma>
2024-11-06 10:33:50 +0100

    -

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: moben-ta <moben-ta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 12:51:29 by moben-ta          #+#    #+#             */
-/*   Updated: 2024/11/01 11:27:27 by moben-ta         ###   ########.fr       */
+/*   Updated: 2024/11/06 10:31:58 by moben-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_calloc(size_t count, size_t size)
 	unsigned char	*tmp;
 
 	if (count == 0 || size == 0)
-		return (NULL);
+		return (malloc(0));
 	tmp = malloc(count * size);
 	if (!tmp)
 		return (NULL);
